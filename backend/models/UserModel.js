b38acd9ed30d2dd2050f58166f68213040c27930
XennGui <xen.luna07@gmail.com>
2025-05-02 20:53:39 -0500

    Creating the model for the user.

---
+++ backend/models/UserModel.js
@@ -0,0 +1,30 @@
+// backend/models/UserModel.js
+
+const Database = require('../config/db');
+
+class UserModel {
+    async crearUsuario(datos) {
+        const query = `
+        INSERT INTO Usuario(nombre, apellidos, email, username, password, rol)
+        VALUES($1, $2, $3, $4, $5, $6)
+        RETURNING id_usuario, username, rol`;
+        return Database.query(query, [
+            datos.nombre,
+            datos.apellidos,
+            datos.email,
+            datos.username,
+            datos.password,
+            datos.rol
+        ]);
+    }
+
+    async buscarPorUsername(username) {
+        const result = await Database.query(
+            'SELECT * FROM Usuario WHERE username = $1',
+            [username]
+        );
+        return result.rows[0];
+    }
+}
+
+module.exports = new UserModel();

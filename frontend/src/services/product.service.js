4e57e6ac70a34ff41db4992542a3773b82f1f7ad
Jaren Ramos <jarenramos234@gmail.com>
2025-05-05 17:53:49 -0500

    updating filters in service to get products

--- frontend/src/services/product.service.js
+++ frontend/src/services/product.service.js
@@ -1,4 +1,7 @@
 //src/services/product.service.js
 import api from './api';
 
-export const obtenerProductos = () => api.get('/productos');
+export const obtenerProductos = (filtros = {}) => {
+    const params = new URLSearchParams(filtros).toString();
+    return api.get(`/productos?${params}`);
+};
